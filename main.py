689e39c3f1869aab31dcbb0e2e1cab4a72d81adc
FireflyIsOnFire <116716243+FireflyIsOnFire@users.noreply.github.com>
2022-12-26 13:05:22 +0100

    none

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import pandas as pd
+import numpy as np
 import mplfinance as mpf
-import datetime
 import pandas_ta as ta
-import pandas_datareader.data as web
+import yfinance as yf
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import datetime
+from datetime import timedelta
 import matplotlib.pyplot as plt
+import backtesting
+from backtesting import Strategy, Backtest
+
 pd.set_option('display.max_columns',None)
 
+df = yf.download('BTC-USD', datetime.datetime(2017,1,1), datetime.date.today())
+df.to_csv('database')
+#pandas datareader is bugged but idk why. here use yfinance instead
 
-df = web.DataReader('BTC-USD', 'yahoo' ,datetime.datetime(2019,1,1), datetime.date.today())
+
 bbands = ta.bbands(df['Close'], length = 20, std = 2.5)
+bbands.to_csv('bollingbands')
 df['SMA20'] = ta.sma(df['Close'], length = 20)
 df['EMA40'] = ta.ema(df['Close'], length = 40)
 df['SMA60'] = ta.sma(df['Close'], length = 60)
 df['SMA200'] = ta.sma(df['Close'], length = 200)
 df['RSI'] = ta.rsi(df['Close'], length = 12)
 macd = ta.macd(df['Close'], fast = 12, slow = 26, signal = 9)
-#dfSPY.dropna(inplace = True)
-macd.to_csv('macd')
-'''print(df)
-print(bbands)
-help(mpf.plot)'''
 
+macd.to_csv('macd')
+
+
+'''
 add_plot = [mpf.make_addplot(bbands.iloc[:,0], type = 'line', color = 'tomato', width = 0.5),
             mpf.make_addplot(bbands.iloc[:,1], type = 'line', color = 'deepskyblue',width = 0.5),
             mpf.make_addplot(bbands.iloc[:,2], type = 'line', color = 'lime', width = 0.5),
@@ -33,6 +43,113 @@ add_plot = [mpf.make_addplot(bbands.iloc[:,0], type = 'line', color = 'tomato', 
             ]
 mpf.plot(df, type = 'candle', style = 'yahoo', volume = True, title = 'Bitcoin/USE Trend', ylabel = 'Preis in USD',
          xlabel = 'Date', show_nontrading = False, addplot = add_plot)
+'''
+
+
+def addemasignal(df, backcandles):
+    emasignal = [0]*len(df)
+    for row in range(backcandles, len(df)):
+        upt = 1
+        dnt = 1
+        for i in range(row - backcandles, row +1):
+            if df['High'][i] >= df['EMA40'][i]:
+                dnt = 0
+            if df['Low'][i] <= df['EMA40'][i]:
+                upt = 0
+        if upt == 1 and dnt == 1:
+            print(f'check trend loop')
+            emasignal[row] = 3
+        elif upt == 1:
+            emasignal[row] = 2
+        elif dnt ==1:
+            emasignal[row] = 1
+    print(f'{emasignal}\n{len(emasignal)}\n{len(df)}')
+    df['EMA_Sig'] = emasignal
+
+addemasignal(df,6)
+
+def addorderlimit(df, percent):
+    ordersignal = [0]*len(df)
+    for i in range(1, len(df)):
+        if df['EMA_Sig'][i] == 2 and df['Close'][i] <= bbands['BBM_20_2.5'][i]: #and df['RSI'][i] >= 50:
+            ordersignal[i] = df['Close'][i] - df['Close'][i] * percent
+        elif df['EMA_Sig'][i] == 1 and df['Close'][i] >= bbands['BBU_20_2.5'][i]: #and df['RSI'] <= 50:
+            ordersignal[i] = df['Close'][i] + df['Close'][i] * percent
+    df['Order_Sig'] = ordersignal
+    print(ordersignal)
+
+addorderlimit(df, 0.03)
+
+def pointposbreak(x):
+    if x['Order_Sig'] != 0:
+        return x['Order_Sig']
+    else:
+        return np.nan
+df['pointposbreak'] = df.apply(lambda row: pointposbreak(row), axis = 1)
+print(df)
+df.to_csv('df')
+
+
+dfplot = df.copy()
+fig = go.Figure(data = [go.Candlestick(x = dfplot.index, open = dfplot['Open'], high = dfplot['High'], low = dfplot['Low'], close = dfplot['Close']),
+                        go.Scatter(x = dfplot.index, y = dfplot.EMA40, line = dict(color = 'orange', width = 1), name = 'EMA40'),
+                        go.Scatter(x= dfplot.index, y = bbands['BBU_20_2.5'], line = dict(color = 'red', width = 1), name = 'bolling_up'),
+                        go.Scatter(x = dfplot.index, y = bbands['BBL_20_2.5'], line = dict(color = 'blue', width = 1), name = 'bolling_low')
+                        ])
+
+fig.add_scatter(x = dfplot.index, y = dfplot['pointposbreak'], mode = 'markers', marker = dict(color = 'MediumPurple', size = 5), name = 'Signal')
+fig.show()
+
+def Signal():
+    return dfplot.Order_Sig
+
+class MyStrategy(Strategy):
+    initsize = 0.5
+    ordertime = []
+    def init(self):
+        super().init()
+        self.signal = self.I(Signal)
+
+    def next(self):
+        super().init()
+        for j in range(0,len(self.orders)):
+            if self.data.index[-1] - self.ordertime[0] > timedelta(days = 5) :#days max to fulfill the trade?
+                self.orders[0].cancel()
+                self.ordertime.pop(0)
+
+        if len(self.trades) >0:
+            if self.data.index[-1] - self.trades[-1].entry_time >= timedelta(days = 10):
+                self.trades[-1].close()
+            if self.trades[-1].is_long and self.data.RSI[-1] >=50:
+                self.trades[-1].close()
+            elif self.trades[-1].is_short and self.data.RSI[-1] <=50:
+                self.trades[-1].close()
+
+        if self.signal != 0 and len(self.trades) ==0 and self.data.EMA_Sig ==2:
+            #cancel previous orders
+            for j in range(0, len(self.orders)):
+                self.orders[0].cancel()
+                self.ordertime.pop(0)
+
+            #add new replacement order
+            self.buy(sl = self.signal / 2, limit = self.signal, size = self.initsize)
+            self.ordertime.append(self.data.index[-1])
+
+        elif self.signal != 0 and len(self.trades) ==0 and self.data.EMA_Sig ==1:
+            # cancel previous orders
+            for j in range(0, len(self.orders)):
+                self.orders[0].cancel()
+                self.ordertime.pop(0)
+
+            #add new replacement order
+            self.sell(sl = self.signal * 2, limit = self.signal, size = self.initsize)
+            self.ordertime.append(self.data.index[-1])
+
+bt = Backtest(dfplot, MyStrategy, cash = 1000000, margin = 1/10, commission = .00)
+stat = bt.run()
+print(stat)
+
+
 
 
 
